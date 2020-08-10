e54161d6f4daad0a919beed54240a17d3cdd95f6
Michelle Macaraeg <macaraegm@gmail.com>
2020-08-10 19:41:45 +0000

    Created homepage.html

--- server.py
+++ server.py
@@ -1,12 +1,50 @@
 """Server for movie ratings app."""
 
-from flask import Flask
+from flask import (Flask, render_template, request, flash, session, redirect)
+
+from model import connect_to_db
+import crud
+
+
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
+app.secret_key = "MAM"
+app.jinja_env.undefined = StrictUndefined
 
 
-# Replace this with routes and view functions!
+@app.route('/')
+def homepage():
+    """Homepage"""
+
+    return render_template('homepage.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
+    connect_to_db(app)
     app.run(host='0.0.0.0', debug=True)
